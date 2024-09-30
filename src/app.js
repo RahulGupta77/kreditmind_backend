dfb7a598f9a4326139a74454036b45f9f906918d
RahulGupta77 <rg3498104@gmail.com>
2024-10-01 03:22:10 +0530

    implemented cors and fixed invalid route error handling

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 const connectDb = require("./config/database");
 const routes = require("./routes");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.options("*", cors());
 
 app.use("/v1", routes);
+
+app.use((req, res, next) => {
+  res.status(404).send("Route not found");
+});
+
+app.use((err, req, res, next) => {
+  res.status(400).send(err.message);
+});
 
 connectDb()
   .then(() => {
